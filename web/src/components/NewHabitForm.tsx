e3d0d04ce2e75ad656e9c694b20c81ba9f40d0d2
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-14 19:04:42 +0100

    chore: get weekdays

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import { Check } from "@phosphor-icons/react";
 import * as Checkbox from "@radix-ui/react-checkbox";
+import { FormEvent, useState } from "react";
 
 const evailableWeekdays = [
   { weekday: "Domingo", key: 0 },
@@ -12,25 +13,50 @@ const evailableWeekdays = [
 ];
 
 export function NewHabitForm() {
+  const [title, seTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  function handleToggleweekday(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithremovedOne = weekDays.filter((day) => day !== weekDay);
+
+      setWeekDays(weekDaysWithremovedOne);
+    } else {
+      const weekDayWithAddwdOne = [...weekDays, weekDay];
+
+      setWeekDays(weekDayWithAddwdOne);
+    }
+  }
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprotimento?
       </label>
+
       <input
         type="text"
         id="title"
         placeholder="ex: Eercícios , dormir bem, ectc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={(e) => seTitle(e.target.value)}
       />
+
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência
       </label>
 
-      <div className="mt-6 flex flex-col gap-3">
+      <div className="mt-3 flex flex-col gap-3">
         {evailableWeekdays.map(({ weekday, key }) => (
-          <Checkbox.Root key={key} className="flex items-center gap-3 group">
+          <Checkbox.Root
+            onCheckedChange={() => handleToggleweekday(key)}
+            key={key}
+            className="flex items-center gap-3 group"
+          >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
